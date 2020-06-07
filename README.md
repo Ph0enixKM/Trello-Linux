532f12efc9dae029edeb1171ce493a71ed6a6016
Phoenix <phoenix@pop-os.localdomain>
2020-06-07 15:43:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align="center">
+    <img src="images/logo.png" width="250">
+</div>
+
+# Trello Linux Desktop
+
+This is an unofficial port of Trello web app to linux executable format. This way you can use trello on your machine without having to use browser.
+
+
+
+![](images/ss.png)
+
+## Download
+
+You can download latest release in github releases. Two formats are available - regular *.tar.gz* and *.deb*
---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const fs = require('fs')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true
+    },
+    icon: path.join(__dirname, 'trello.png')
+  })
+
+  // and load the index.html of the app.
+  win.setMenu(null)
+  win.loadFile('./src/dist/index.html')
+}
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
